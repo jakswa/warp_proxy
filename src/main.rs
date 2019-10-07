ce82d900b7f5b140d4b43a502752acd9b77cd302
Jake Swanson <jakswa@gmail.com>
2019-10-07 17:26:39 -0400

    remove string.clone(), replace with bytes.clone()
    - Bytes crate provides a reference-counted generic data holder which you
    can build http::Response objects with easily
    - credit for this idea comes from https://github.com/seanmonstar/warp/issues/283 :sparkling_heart:
    - fixes #1

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,21 @@ extern crate serde;
 extern crate serde_derive;
 extern crate warp;
 extern crate reqwest;
+extern crate bytes;
 use std::time::{Duration, Instant};
 //use std::env;
 
 use std::sync::{Arc, RwLock};
 //use warp::{http::StatusCode, Filter};
-use warp::{Filter};
+use warp::{Filter, http::Response};
+use bytes::Bytes;
 
 type Store = Arc<RwLock<TimedString>>;
 
 struct TimedString {
     time: Instant,
     url: String,
-    text: String,
+    text: Bytes,
 }
 
 /// Provides a RESTful web server managing some Todos.
@@ -32,7 +34,7 @@ fn main() {
     let bus_cache = TimedString {
         time: Instant::now() - Duration::from_secs(30),
         url: String::from("http://developer.itsmarta.com/BRDRestService/RestBusRealTimeService/GetAllBus"),
-        text: String::from("<unused>")
+        text: Bytes::from(&b"<unused>"[..])
     };
     let bus_cache = Arc::new(RwLock::new(bus_cache));
     let bus_cache = warp::any().map(move || bus_cache.clone());
@@ -62,7 +64,7 @@ fn cache_visit(cache: Store) -> impl warp::Reply {
         // should be very fast
         let timed_string = cache.read().unwrap();
         if Instant::now() < timed_string.time {
-            return timed_string.text.clone();
+            return Response::new(timed_string.text.clone());
         }
     }
 
@@ -71,11 +73,12 @@ fn cache_visit(cache: Store) -> impl warp::Reply {
     // check if someone else acquired the write lock while we were blocked
     // and updated the cache already, so just return result
     if Instant::now() < timed_string.time {
-        return timed_string.text.clone();
+        return Response::new(timed_string.text.clone());
     }
 
     // otherwise we have to update the cache, slowest case.
     timed_string.time = Instant::now() + Duration::from_secs(10);
-    timed_string.text = reqwest::get(&timed_string.url).unwrap().text().unwrap();
-    timed_string.text.clone()
+    let resp_text = reqwest::get(&timed_string.url).unwrap().text().unwrap();
+    timed_string.text = Bytes::from(resp_text);
+    Response::new(timed_string.text.clone())
 }
